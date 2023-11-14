c7b694842d9d7a7ad5f7ccc60f25252208567f53
NohaGlal <engnohy@gmail.com>
2023-11-14 12:53:06 -0800

    find func

--- file_tools_emad.c
+++ file_tools_emad.c
@@ -72,3 +72,56 @@ int parse_line(char *buffer, int line_number, int format)
 	find_func(opcode, value, line_number, format);
 	return (format);
 }
+
+
+
+/**
+ * find_func - a func is to find the appropriate function for the opcode
+ * @opcode:  a pntr opcode
+ * @value: a pntr character type argument of opcode
+ * @format:  an integer type storage format.
+ * If (0) nodes will be entered as a stack
+ * @ln: an integer type and line number
+ * if (1) node will entered as a queue.
+ * Return: nothing
+ */
+
+
+void find_func(char *opcode, char *value, int ln, int format)
+{
+	int i;
+	int flag;
+
+	instruction_t func_list[] = {
+		{"push", add_to_stack},
+		{"pall", print_stack},
+		{"pint", print_top},
+		{"pop", pop_top},
+		{"nop", nop},
+		{"swap", swap_nodes},
+		{"add", add_nodes},
+		{"sub", sub_nodes},
+		{"div", div_nodes},
+		{"mul", mul_nodes},
+		{"mod", mod_nodes},
+		{"pchar", print_char},
+		{"pstr", print_str},
+		{"rotl", rotl},
+		{"rotr", rotr},
+		{NULL, NULL}
+	};
+
+	if (opcode[0] == '#')
+		return;
+
+	for (flag = 1, i = 0; func_list[i].opcode != NULL; i++)
+	{
+		if (strcmp(opcode, func_list[i].opcode) == 0)
+		{
+			call_fun(func_list[i].f, opcode, value, ln, format);
+			flag = 0;
+		}
+	}
+	if (flag == 1)
+		err(3, ln, opcode);
+}
