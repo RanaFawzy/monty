a81d5393f9ec78985e453c499c6a813040129b0e
NohaGlal <engnohy@gmail.com>
2023-11-14 04:57:32 -0800

    string

--- errors_emad.c
+++ errors_emad.c
@@ -95,3 +95,34 @@ void err(int error_code, ...)
 	free_nodes();
 	exit(EXIT_FAILURE);
 }
+
+
+/**
+ * string_err - a function  to deal with errors
+ * @error_code: error codes are integer type and are as following:
+ * (10) ~> case 10 is the number inside node is outside ASCII bounds
+ * (11) ~> case 11 is  stack is empty
+ */
+
+
+void string_err(int error_code, ...)
+{
+va_list ag;
+int l_num;
+
+va_start(ag, error_code);
+l_num = va_arg(ag, int);
+switch (error_code)
+{
+	case 10:
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", l_num);
+		break;
+	case 11:
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", l_num);
+		break;
+	default:
+		break;
+}
+free_nodes();
+exit(EXIT_FAILURE);
+}
