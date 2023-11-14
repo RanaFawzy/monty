aee759c7ea7096bbf41705fbeaa81badf2f8b79f
NohaGlal <engnohy@gmail.com>
2023-11-14 12:46:51 -0800

    pars

--- file_tools_emad.c
+++ file_tools_emad.c
@@ -38,3 +38,37 @@ if (file_name == NULL || fd == NULL)
 read_file(fd);
 fclose(fd);
 }
+
+/**
+ * parse_line - funct  to separate each line into tokens to determine
+ * which function to call
+ * @buffer: pntr that points to line from  file
+ * @line_number: the line number
+ * @format:  integer type storage format.
+ * If (0) node will be entered as a stack.
+ * if (1) node will be entered as a queue.
+ * Return: 0 (success)
+ *       if the opcode is stack. or 1 if queue.
+ */
+
+int parse_line(char *buffer, int line_number, int format)
+{
+	char *opcode, *value;
+	const char *delim = "\n ";
+
+	if (buffer == NULL)
+		err(4);
+
+	opcode = strtok(buffer, delim);
+	if (opcode == NULL)
+		return (format);
+	value = strtok(NULL, delim);
+
+	if (strcmp(opcode, "stack") == 0)
+		return (0);
+	if (strcmp(opcode, "queue") == 0)
+		return (1);
+
+	find_func(opcode, value, line_number, format);
+	return (format);
+}
