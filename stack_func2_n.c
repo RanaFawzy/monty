13f325fce911dbcab8ded3f1587e782c342e1740
NohaGlal <engnohy@gmail.com>
2023-11-15 11:02:24 -0800

    div nodes

--- stack_func2_n.c
+++ stack_func2_n.c
@@ -85,3 +85,28 @@ void add_nodes(stack_t **stack, unsigned int line_number)
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
+
+
+
+/**
+ * div_nodes - a function used to add the top 2 elements of the stack
+ * @stack: pntr points to top node of the stack.
+ * @line_number: an integer type and  represent a line number of opcode.
+ */
+
+
+void div_nodes(stack_t **stack, unsigned int line_number)
+{
+int sum;
+
+if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	more_err(8, line_number, "div");
+
+if ((*stack)->n == 0)
+	more_err(9, line_number);
+(*stack) = (*stack)->next;
+sum = (*stack)->n / (*stack)->prev->n;
+(*stack)->n = sum;
+free((*stack)->prev);
+(*stack)->prev = NULL;
+}
