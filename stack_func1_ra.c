5ad66b29ba07e2f1b0ce1a55aa92db0e56305749
RanaFawzy <rororona91@gmail.com>
2023-11-15 05:01:03 -0800

    add stack_func1_ra.c

--- stack_func1_ra.c
+++ stack_func1_ra.c
@@ -48,3 +48,24 @@ if (*stack != NULL)
 (*stack)->prev = NULL;
 free(tmp);
 }
+/**
+ * add_to_stack - this function is to add node to stacks.
+ * @ln: Interger thisis int type and representing line number of opcodee.
+ * @new_node: this is pointer that points to new nodee.
+ *
+ */
+void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+stack_t *tmp;
+if (new_node == NULL || *new_node == NULL)
+exit(EXIT_FAILURE);
+if (head == NULL)
+{
+head = *new_node;
+return;
+}
+tmp = head;
+head = *new_node;
+head->next = tmp;
+tmp->prev = head;
+}
