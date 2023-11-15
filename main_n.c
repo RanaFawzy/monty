95dd06002a3a6455df80bb6c3e25ce42d841bc00
NohaGlal <engnohy@gmail.com>
2023-11-15 02:29:12 -0800

    free node

--- main_n.c
+++ main_n.c
@@ -20,3 +20,56 @@ node->prev = NULL;
 node->n = n;
 return (node);
 }
+
+
+
+/**
+ * free_nodes - a function using to free nodes in a stack
+ */
+
+void free_nodes(void)
+{
+stack_t *tmp;
+
+if (head == NULL)
+	return;
+
+while (head != NULL)
+{
+	tmp = head;
+	head = head->next;
+	free(tmp);
+}
+}
+
+
+
+
+/**
+ * add_to_queue - a func using to add  node to the queue
+ * @new_node: pntr to point to the new node
+ * @ln: an integer type and line number of the opcode
+ */
+
+
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+
+}
+stack_t *head = NULL;
+
