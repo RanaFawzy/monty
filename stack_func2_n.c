96e415c29d56ba41ab90345cd19553706b84a016
NohaGlal <engnohy@gmail.com>
2023-11-15 10:59:19 -0800

    add node

--- stack_func2_n.c
+++ stack_func2_n.c
@@ -62,3 +62,26 @@ void nop(stack_t **stack, unsigned int line_number)
 (void)stack;
 (void)line_number;
 }
+
+
+
+/**
+ * add_nodes - a function used to add top 2 elements of the stack
+ * @stack: pntr points to the top node of the stack
+ * @line_number: an integer type and  represent a line number of  opcode
+ */
+
+
+void add_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "add");
+
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n + (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
