4804a6fa62f293df5a0c0274f2dafe575c4971a8
NohaGlal <engnohy@gmail.com>
2023-11-16 02:48:02 -0800

    print string

--- stack_strings_n.c
+++ stack_strings_n.c
@@ -19,3 +19,35 @@ if (ascii < 0 || ascii > 127)
 	string_err(10, line_number);
 printf("%c\n", ascii);
 }
+
+
+
+/**
+ * print_str - a function using to Print  string
+ * @stack: pntr points to the  top node of the stack.
+ * @ln: an integer type n represent the line number of the opcode
+ */
+
+
+void print_str(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	int ascii;
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("\n");
+		return;
+	}
+
+	tmp = *stack;
+	while (tmp != NULL)
+	{
+		ascii = tmp->n;
+		if (ascii <= 0 || ascii > 127)
+			break;
+		printf("%c", ascii);
+		tmp = tmp->next;
+	}
+	printf("\n");
+}
